83729c1d1d0d2c53437754aa443419f31ce76a5e
Ashlen-FPT <107978815+Ashlen-FPT@users.noreply.github.com>
2022-10-11 09:04:20 +0200

    Add project files.

---
+++ GC/GCBooking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralCargoSystem.Models.GC
+{
+    public class GCBooking
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
