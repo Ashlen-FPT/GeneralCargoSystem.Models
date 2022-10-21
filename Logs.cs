095dcf51842d8b80f45dce8fd07b29fa8cb0659d
Ashlen-FPT <107978815+Ashlen-FPT@users.noreply.github.com>
2022-10-21 11:59:17 +0200

    Added Logs Class

---
+++ Logs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralCargoSystem.Models
+{
+    public  class Logs
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserEmail { get; set; }
+
+        public string LogType { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string AffectedTable { get; set; }
+    }
+}
